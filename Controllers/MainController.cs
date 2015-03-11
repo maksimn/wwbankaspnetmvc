cc2d2f94bedf083ffede52ac68c7e0eb7ab1bfe8
maksimn <maksim1989sovunion@yandex.ru>
2015-03-12 00:59:40 +0300

    My first substantial ASP.NET MVC code

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Web.Mvc;
+using WildWestBankApp.Models;
 
 namespace WildWestBankApp.Controllers {
     public class MainController : Controller {
+        private DataRepository repository;
+        public MainController() {
+            repository = new DataRepository();
+            repository.LoadCustomersFromDataFile();
+        }
         public ActionResult Customers() {
-            return View();
+            return View(repository.Customers);
         }
     }
 }
--- Models/DataRepository.cs
+++ Models/DataRepository.cs
@@ -1,15 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace WildWestBankApp.Models {
     public class DataRepository {
+        private String rootFolder = AppDomain.CurrentDomain.BaseDirectory;
         private List<Customer> customers;
         private List<Account> accounts;
-        public void LoadCustomersFromDataFile() {
-
+        public void LoadCustomersFromDataFile() {            
+            String customersFilePath = rootFolder + @"\data\Customer.csv";
+            customers = new List<Customer>();
+            using(StreamReader file = new StreamReader(customersFilePath)) {
+                String line;
+                String[] rowField;
+                file.ReadLine();
+                while ((line = file.ReadLine()) != null) {
+                    rowField = line.Split(new Char[] { ';' });
+                    Customer customer = new Customer();
+                    customer.ID = Convert.ToInt32(rowField[0]);
+                    customer.Name = rowField[1];
+                    customer.Address = rowField[2];
+                    Int32 year = Convert.ToInt32(rowField[3].Substring(0, 4));
+                    Int32 month = Convert.ToInt32(rowField[3].Substring(4, 2));
+                    Int32 day = Convert.ToInt32(rowField[3].Substring(6, 2));
+                    customer.BirthDay = new DateTime(year, month, day);
+                    customers.Add(customer);
+                }
+            }
         }
         public void LoadAccountsFromDataFile() {
-
+            String _accountsFilePath = rootFolder + @"\data\Account.csv";
         }
         public List<Customer> Customers {
             get { 
