8795ca91bebb52d6b3c2c2cbf02414b57384d183
maksimn <maksim1989sovunion@yandex.ru>
2015-05-31 13:32:39 +0300

    Migration data from csv to db completed

--- App_Data/Program.cs
+++ App_Data/Program.cs
@@ -12,13 +12,31 @@ public class Customer {
     public String Address { get; set; }
     public DateTime BirthDay { get; set; }
 }
+public class Account { 
+    public Int32 AccountID { get; set; } 
+    public Int32 CustomerID { get; set; }
+    public Decimal Money { get; set; }
+}
+public class Transaction {
+    public Int32 ID { get; set; }
+    public Int32 FromAccountID { get; set; }
+    public Int32 ToAccountID { get; set; }
+    public TransactionType Type { get; set; }
+    public Decimal Amount { get; set; }
+    public DateTime DateTime { get; set; }
+}
+public enum TransactionType { Transfer = 1, Add = 2, Withdraw = 3 }
 
 public class Program {
     private static readonly String csvFolder = 
         @"D:\PrevRabStol\MyProgramming\ASP.NET MVC\WildWestBankApp\for_source_control\data\";
     private static readonly String customerCsvFileName = "Customer.csv";
+    private static readonly String accountCsvFileName = "Account.csv", transactionCsvFileName = "Transaction.csv";
+
 
     private static List<Customer> customers;
+    private static List<Account> accounts;
+    private static List<Transaction> transactionList;
 
     private static String ConvertToString(Customer customer) {
         String temp = "0" + customer.BirthDay.Day;
@@ -27,6 +45,10 @@ public class Program {
         String strMonth = temp.Substring(temp.Length - 2);
         return String.Format("{0};{1};{2};{3}{4}{5}", customer.ID, customer.Name, customer.Address,
             customer.BirthDay.Year, strMonth, strDay);
+    }
+
+    private static String ConvertToString(Account account) {
+        return String.Format("{0};{1};{2}", account.AccountID, account.CustomerID, account.Money);
     }
 
     private static String DateTimeToNeutralString(DateTime datetime) {
@@ -59,13 +81,12 @@ public class Program {
         }
     }
 
-    public static void Main() {
-        LoadCustomersFromDataFile(); // Список клиентов из файла загружается успешно.        
+    private static void InsertCustomersIntoDB() {               
         String connName = "WWBankConnectionString";
         ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[connName];
         DbProviderFactory factory = DbProviderFactories.GetFactory(settings.ProviderName);
         using (DbConnection conn = factory.CreateConnection()) {
-            conn.ConnectionString = settings.ConnectionString; 
+            conn.ConnectionString = settings.ConnectionString;
             conn.Open();
             foreach (var c in customers) {
                 String insertQuery =
@@ -77,5 +98,95 @@ public class Program {
             }
             conn.Dispose();
         } 
+    }
+
+    private static void LoadAccountsFromDataFile() {
+        accounts = new List<Account>();
+        using (StreamReader file = new StreamReader(csvFolder + accountCsvFileName)) {
+            String line;
+            String[] rowField;
+            file.ReadLine();
+            while ((line = file.ReadLine()) != null) {
+                rowField = line.Split(new Char[] { ';' });
+                Account account = new Account() {
+                    AccountID = Convert.ToInt32(rowField[0]),
+                    CustomerID = Convert.ToInt32(rowField[1]),
+                    Money = Convert.ToDecimal(rowField[2])
+                };
+                accounts.Add(account);
+            }
+            file.Dispose();
+        }
+    }
+
+    private static void InsertAccountsIntoDB() {
+        String connName = "WWBankConnectionString";
+        ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[connName];
+        DbProviderFactory factory = DbProviderFactories.GetFactory(settings.ProviderName);
+        using (DbConnection conn = factory.CreateConnection()) {
+            conn.ConnectionString = settings.ConnectionString;
+            conn.Open();
+            foreach (var a in accounts) {
+                String insertQuery =
+                    String.Format("INSERT INTO [dbo].[Accounts] (AccountID, CustomerID, Money) VALUES ({0}, '{1}', '{2}')",
+                        a.AccountID, a.CustomerID, a.Money.ToString().Replace(',', '.'));
+                SqlCommand cmd = new SqlCommand(insertQuery, (SqlConnection)conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            conn.Dispose();
+        }
+    }
+
+    private static void LoadTransactionListFromFile() {
+        transactionList = new List<Transaction>();
+        using (StreamReader file = new StreamReader(csvFolder + transactionCsvFileName)) {
+            String line;
+            String[] rowField;
+            file.ReadLine();
+            while ((line = file.ReadLine()) != null) {
+                rowField = line.Split(new Char[] { ';' });
+                Transaction transaction = new Transaction();
+                transaction.ID = Convert.ToInt32(rowField[0]);
+                transaction.FromAccountID = Convert.ToInt32(rowField[1]);
+                transaction.ToAccountID = Convert.ToInt32(rowField[2]);
+                transaction.Type = (TransactionType)Convert.ToInt32(rowField[3]);
+                transaction.Amount = Convert.ToDecimal(rowField[4]);
+                transaction.DateTime = Convert.ToDateTime(rowField[5]);
+                transactionList.Add(transaction);
+            }
+            file.Dispose();
+        }
+    }
+    private static void InsertTransactionsIntoDB() {
+        String connName = "WWBankConnectionString";
+        ConnectionStringSettings settings = ConfigurationManager.ConnectionStrings[connName];
+        DbProviderFactory factory = DbProviderFactories.GetFactory(settings.ProviderName);
+        using (DbConnection conn = factory.CreateConnection()) {
+            conn.ConnectionString = settings.ConnectionString;
+            conn.Open();
+            foreach (var t in transactionList) {
+                String insertQuery =
+                    String.Format("INSERT INTO [dbo].[Transactions] (ID, AccountFromId, AccountToId, Type, Amount, DateTime) VALUES ({0}, {1}, {2}, {3}, {4}, '{5}')",
+                        t.ID, t.FromAccountID, t.ToAccountID, (int)(t.Type), t.Amount.ToString().Replace(',', '.'), t.DateTime.ToString("yyyy-MM-dd HH:mm:ss"));
+                SqlCommand cmd = new SqlCommand(insertQuery, (SqlConnection)conn);
+                cmd.CommandType = CommandType.Text;
+                cmd.ExecuteNonQuery();
+            }
+            conn.Dispose();
+        }
+    }
+    static private String ConvertToString(Transaction transaction) {
+        return String.Format("{0};{1};{2};{3};{4};{5}", transaction.ID, transaction.FromAccountID,
+            transaction.ToAccountID, (Int32)transaction.Type, transaction.Amount, transaction.DateTime);
+    }
+
+    public static void Main() {
+        //LoadCustomersFromDataFile(); // Список клиентов из файла загружается успешно. 
+        //InsertCustomersIntoDB();
+        //LoadAccountsFromDataFile(); // SUCCESS
+        //InsertAccountsIntoDB();
+        //LoadTransactionListFromFile(); 
+        //InsertTransactionsIntoDB();
     }
 }
